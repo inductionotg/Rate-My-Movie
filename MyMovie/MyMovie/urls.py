d650864da0c7e19b705cdd07cfecd01edb96e3f5
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-18 20:15:14 +0530

    Update for routers

--- MyMovie/MyMovie/urls.py
+++ MyMovie/MyMovie/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
-from TestApp.views import RegisterAPI,LoginAPI
+from django.urls import path, include
+from TestApp.views import RegisterAPI, LoginAPI
 from knox import views as knox_views
-from TestApp import  views
+from TestApp import views
 from rest_framework import routers
-router=routers.DefaultRouter()
-router.register('api/movie',views.UserMovieView)
+
+router = routers.DefaultRouter()
+router.register('api/movie', views.UserMovieView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +30,5 @@ urlpatterns = [
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-    path('',include(router.urls))
+    path('', include(router.urls))
 ]
