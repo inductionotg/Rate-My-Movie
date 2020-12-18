ea07c6b7ba73b9163b28b64928634c535917731f
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-18 19:58:53 +0530

    Add UserSerializer

--- MyMovie/TestApp/serializer.py
+++ MyMovie/TestApp/serializer.py
@@ -21,3 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
         return user
+class UserMovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=UserMovie
+        fields='__all__'
+
