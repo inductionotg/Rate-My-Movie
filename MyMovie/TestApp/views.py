52660aa142851ee46d1dc896e9dfd8f18a56670e
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-18 19:49:06 +0530

    Add RegisterApi and LoginApi

--- MyMovie/TestApp/views.py
+++ MyMovie/TestApp/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from knox.models import AuthToken
+from TestApp.serializer import UserSerializer, RegisterSerializer, UserMovieSerializer
+from django.contrib.auth import login
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from rest_framework.viewsets import ModelViewSet
+from TestApp.models import *
+from TestApp.permissions import *
+from django.shortcuts import redirect
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
+        return redirect('http://127.0.0.1:8000/api/movie/')
+        # return HttpResponseRedirect(redirect_to='api/movie')
 
 # Create your views here.
