0420f2f2c6d6c0a28c67cf4b716a167af2a0792e
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-18 19:55:20 +0530

    Add models

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class UserMovie(models.Model):
+    Movie = models.CharField(max_length=64)
+    Title = models.CharField(max_length=128)
+    Rating = models.IntegerField()
