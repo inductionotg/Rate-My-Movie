c9a4c9ac58c03203e51d9e0d2be511856faa6a9e
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-21 11:35:34 +0530

    Apply OneToOneField

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+class RatingRates(models.Model):
+    Rating = models.BigIntegerField(default=0)
+
 class UserMovie(models.Model):
     Movie = models.CharField(max_length=64)
     Title = models.CharField(max_length=128)
-    Rating = models.IntegerField()
+    rates = models.OneToOneField(RatingRates, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.Movie
