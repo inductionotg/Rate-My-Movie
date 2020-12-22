544dcb1d8059c0f7ce2759911ac2e83f658e85ed
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-22 10:44:50 +0530

    Add another model

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -3,12 +3,17 @@ from django.db import models
 
 # Create your models here.
 class RatingRates(models.Model):
-    Rating = models.BigIntegerField(default=0)
+    Rating = models.IntegerField()
 
 class UserMovie(models.Model):
     Movie = models.CharField(max_length=64)
     Title = models.CharField(max_length=128)
-    rates = models.OneToOneField(RatingRates, on_delete=models.CASCADE, null=True)
+    #rates = models.OneToOneField(RatingRates, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.Movie
+
+class JoinModel(models.Model):
+    Movie=models.CharField(max_length=64)
+    Title=models.CharField(max_length=128)
+    Rating=models.IntegerField()
