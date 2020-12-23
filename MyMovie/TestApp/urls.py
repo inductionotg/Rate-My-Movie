dcd48ee1f602984c750ea2bcf80267abb5fa10a6
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-23 09:35:18 +0530

    Changes Related to Movie and login urls

---
+++ MyMovie/TestApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+
+from knox.views import LogoutView
+
+from TestApp.views import UserAPIView, RegisterAPIView, LoginAPIView, MovieAPIView, RatingAPIView
+
+urlpatterns = [
+    path('', include('knox.urls')),
+    path('user', UserAPIView.as_view()),
+    path('register', RegisterAPIView.as_view()),
+    path('login', LoginAPIView.as_view()),
+    path('logout', LogoutView.as_view(), name='knox_logout'),
+    path('movies', MovieAPIView.as_view()),
+    path('rating', RatingAPIView.as_view())
+]
