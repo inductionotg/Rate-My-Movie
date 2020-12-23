04ad49dbe7f545b7c2aa6379d698bca51d965183
inductionotg <ritesh2000.sinha@gmail.com>
2020-12-23 09:41:17 +0530

    Update the movie and other serializers

--- MyMovie/TestApp/serializer.py
+++ MyMovie/TestApp/serializer.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+"""from rest_framework import serializers
 from django.contrib.auth.models import User
 from TestApp.models import UserMovie, RatingRates, JoinModel
 
@@ -34,13 +34,60 @@ class RateSerializer(serializers.ModelSerializer):
         fields = ['id', 'Rating', ]
 
 
-"""class RateSerializer(serializers.ModelSerializer):
+class RateSerializer(serializers.ModelSerializer):
     class Meta:
         model = RatingRates
-        fields = ('Rating',)"""
+        fields = ('Rating',)
 
 
 class JoinModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = JoinModel
         fields = ['id', 'Movie', 'Rating', 'Title']
+"""
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+from TestApp.models import Movie, Rating
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Wrong Credentials")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'director']
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['id', 'rating']
