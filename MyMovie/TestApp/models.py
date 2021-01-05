8b0524af939652c8a07de06f369d3bb9630f87e6
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-05 20:14:44 +0530

    Add foriegnkey to models

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -24,12 +24,10 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=128)
     director = models.CharField(max_length=128)
-    added_by = models.ForeignKey(User,related_name="movies", on_delete=models.CASCADE, null=True)
+    added_by = models.ForeignKey(User, related_name="movies", on_delete=models.CASCADE, null=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
     # rating=models.IntegerField()
@@ -37,8 +35,11 @@ class Movie(models.Model):
         db_table = "Movie"
 
 class Rating(models.Model):
-    rating = models.IntegerField(validators=[MinValueValidator(0),
-                                       MaxValueValidator(5)])
+    movies = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_rating')
 
     class Meta:
         db_table = "Rating"
+
+
