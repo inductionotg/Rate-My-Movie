9c7f725c5e1518457151315f845865c76f6910ea
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-05 23:51:31 +0530

    Add changes related to validation

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -27,7 +27,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 class Movie(models.Model):
     title = models.CharField(max_length=128)
     director = models.CharField(max_length=128)
-    added_by = models.ForeignKey(User, related_name="movies", on_delete=models.CASCADE, null=True)
+    added_by = models.ForeignKey(User, related_name="movie", on_delete=models.CASCADE, null=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
     # rating=models.IntegerField()
@@ -35,7 +35,7 @@ class Movie(models.Model):
         db_table = "Movie"
 
 class Rating(models.Model):
-    movies = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_rating')
 
