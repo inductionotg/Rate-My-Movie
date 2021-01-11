d9b441da39f44171e32e09e4f148cbb28f69f4bd
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-11 10:57:25 +0530

    Remove unnecessary code

--- MyMovie/TestApp/models.py
+++ MyMovie/TestApp/models.py
@@ -1,24 +1,3 @@
-'''from django.db import models
-
-
-# Create your models here.
-class RatingRates(models.Model):
-    Rating = models.IntegerField()
-
-class UserMovie(models.Model):
-    Movie = models.CharField(max_length=64)
-    Title = models.CharField(max_length=128)
-    #rates = models.OneToOneField(RatingRates, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.Movie
-
-class JoinModel(models.Model):
-    Movie=models.CharField(max_length=64)
-    Title=models.CharField(max_length=128)
-    Rating=models.IntegerField()
-'''
-
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -30,16 +9,15 @@ class Movie(models.Model):
     added_by = models.ForeignKey(User, related_name="movie", on_delete=models.CASCADE, null=True)
     added_at = models.DateTimeField(auto_now_add=True)
 
-    # rating=models.IntegerField()
+
     class Meta:
         db_table = "Movie"
 
+
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(5)])
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_rating')
 
     class Meta:
         db_table = "Rating"
-
-
