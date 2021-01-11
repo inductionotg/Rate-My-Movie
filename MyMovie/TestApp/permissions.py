44b1355f5bd395d81e943841742c021a5e3ce23b
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-11 09:28:49 +0530

    Add changes related to userpermission class

--- MyMovie/TestApp/permissions.py
+++ MyMovie/TestApp/permissions.py
@@ -2,12 +2,12 @@ from rest_framework import permissions
 from django.http import Http404
 from TestApp.models import *
 from rest_framework.permissions import BasePermission, SAFE_METHODS
+
 """
 class UserPermission(BasePermission):
-    
-    Allow users who have not added the movie to rate the movie once.
-    
 
+    
+    message= "Allow users who have not added the movie to rate the movie once."
     def has_permission(self, request, view):
         try:
             movie = Movie.objects.get(pk=request.data['movie'])
@@ -21,11 +21,14 @@ class UserPermission(BasePermission):
             except Rating.DoesNotExist:
                 return True
         return False
-    """
+"""
+
+
 class UserPermission(BasePermission):
-    message="You cant rate it "
+    message = "You cant rate it "
+
     def has_permission(self, request, view):
-        movie =Movie.objects.get(pk=request.data['movie'])
+        movie = Movie.objects.get(pk=request.data['movie'])
         user = request.data['user']
         ratings = Rating.objects.filter(movie=request.data['movie'], user=user)
         if movie.added_by == user:
@@ -34,5 +37,3 @@ class UserPermission(BasePermission):
             return True
         else:
             return False
-
-
