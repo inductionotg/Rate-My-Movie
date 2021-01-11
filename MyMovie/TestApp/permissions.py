534e1ffba9228a141a30be19507016095decd372
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-11 10:58:06 +0530

    Remove unnecessary code from permissions

--- MyMovie/TestApp/permissions.py
+++ MyMovie/TestApp/permissions.py
@@ -3,26 +3,6 @@ from django.http import Http404
 from TestApp.models import *
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-"""
-class UserPermission(BasePermission):
-
-    
-    message= "Allow users who have not added the movie to rate the movie once."
-    def has_permission(self, request, view):
-        try:
-            movie = Movie.objects.get(pk=request.data['movie'])
-        except Movie.DoesNotExist:
-            raise Http404
-
-        if request.user != movie.added_by:
-            try:
-                Rating.objects.get(movie=request.data['movie'], user=request.user)
-                return False
-            except Rating.DoesNotExist:
-                return True
-        return False
-"""
-
 
 class UserPermission(BasePermission):
     message = "You cant rate it "
