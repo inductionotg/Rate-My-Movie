671cfb84dfa4e7000133dea0cfe74141c9a3e1e9
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-15 19:15:54 +0530

    Add schedule and crontab

--- MyMovie/MyMovie/celery.py
+++ MyMovie/MyMovie/celery.py
@@ -2,10 +2,22 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
+from celery.schedules import crontab
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyMovie.settings')
 
 app = Celery('MyMovie')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)                                                                                         
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'TestApp.tasks.send_rating',
+        'schedule':crontab(),
+        #'args': ('ritesh2000.sinha@gmail.com',)
+    }
+
+}
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+@app.task(bind=True)
+def debug_task(self):
+    print('Request:{0!r}'.format(self.request))
