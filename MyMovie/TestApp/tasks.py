05a3e409325df4902232f1fd08f5d27ab4cfb9b5
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-15 19:24:45 +0530

    Add shared task for email

--- MyMovie/TestApp/tasks.py
+++ MyMovie/TestApp/tasks.py
@@ -2,15 +2,19 @@ from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
 from TestApp.models import Movie, Rating,User
-
+from django.db.models import Avg
 
 @shared_task
 def send_rating():
-
-    rating = Rating.objects.all()
-    body = rating.rating
+    body= Rating.objects.all().aggregate(Avg('rating'))
     to = [User.email, ]
     sender = settings.EMAIL_HOST_USER
     subject = "Ratings of the movie are"
     send_mail(subject, body, sender, to, fail_silently=False)
+
+@shared_task
+def send_email(email):
+    u=Rating.objects.all().aggregate(Avg('rating'))
+    print(f'rating {u}')
+    print(f'A sample mail {email}')
 
