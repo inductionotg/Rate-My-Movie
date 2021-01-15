4e20c2fe408b228abe6b2111418f09d670b4f321
inductionotg <ritesh2000.sinha@gmail.com>
2021-01-15 19:19:16 +0530

    Add django_celery_beat

--- MyMovie/MyMovie/settings.py
+++ MyMovie/MyMovie/settings.py
@@ -15,7 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+import djcelery
+djcelery.setup_loader()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'djcelery',
+    'django_celery_beat',
     'TestApp'
     
 ]
